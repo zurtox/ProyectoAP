1d0ed62e03be95e27e0819d1b5d2959030588953
Sully-2403 <xavi.diaz.j@gmail.com>
2024-05-20 22:20:42 -0600

    modified selected movie

--- front-AP/src/app/shared/pages/actor-box/actor-box.component.ts
+++ front-AP/src/app/shared/pages/actor-box/actor-box.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-actor-box',
+  selector: 'actor-box',
   standalone: true,
   imports: [],
   templateUrl: './actor-box.component.html',
   styleUrl: './actor-box.component.css'
 })
 export class ActorBoxComponent {
-
+  actorName: string = 'NOLAN';
+  actorRole: string = 'Director';
 }
--- front-AP/src/app/shared/pages/selected/selected-movie-page.component.ts
+++ front-AP/src/app/shared/pages/selected/selected-movie-page.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { ActorBoxComponent } from '../actor-box/actor-box.component';
 
 @Component({
   selector: 'app-selected-movie-page',
   standalone: true,
-  imports: [NavbarComponent
+  imports: [NavbarComponent,
+            ActorBoxComponent
   ],
   templateUrl: './selected-movie-page.component.html',
   styleUrl: './selected-movie-page.component.css'
 })
 export class SelectedMoviePageComponent {
-
+  actorBox: ActorBoxComponent[]=[new ActorBoxComponent(), new ActorBoxComponent(), new ActorBoxComponent()]
   movieTitle: string = 'Openheimer';
   movieReleaseDate: string = '24 Febrero 2024';
   movieDuration: string = '2 horas 30 minutos';
