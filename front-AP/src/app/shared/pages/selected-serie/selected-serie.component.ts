100247c9400dc27f1806de520c497eec46992638
Sully-2403 <xavi.diaz.j@gmail.com>
2024-05-24 17:41:40 -0600

    selected-movie, comments responsive

--- front-AP/src/app/shared/pages/selected-serie/selected-serie.component.ts
+++ front-AP/src/app/shared/pages/selected-serie/selected-serie.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ActorBoxComponent } from '../../components/actor-box/actor-box.component';
 import { EpisodeBoxComponent } from '../../components/episode-box/episode-box.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { DropdownMenuComponent } from '../../components/dropdown-menu/dropdown-menu.component';
 
 @Component({
   selector: 'app-selected-serie',
   standalone: true,
-  imports: [ActorBoxComponent, NavbarComponent, EpisodeBoxComponent],
+  imports: [ActorBoxComponent, NavbarComponent, EpisodeBoxComponent, DropdownMenuComponent],
   templateUrl: './selected-serie.component.html',
   styleUrl: './selected-serie.component.css'
 })
@@ -18,4 +19,10 @@ export class SelectedSerieComponent {
   movieDuration: string = '2 horas 30 minutos';
   movieSynopsis: string = 'Lorem ipsum dolor sit amet consectetur. Sed consequat ornare varius mauris facilisi aliquam a vel arcu. Rutrum vestibulum venenatis viverra proin at massa odio.Lorem ipsum dolor sit amet consectetur. Sed consequat ornare varius mauris facilisi aliquam a vel arcu. Rutrum vestibulum venenatis viverra proin at massa odio.Lorem ipsum dolor sit amet consectetur. Sed consequat ornare varius mauris facilisi aliquam a vel arcu. Rutrum vestibulum venenatis viverra proin at massa odio. Lorem ipsum dolor sit amet consectetur. Sed consequat ornare varius mauris facilisi aliquam a vel arcu. Rutrum vestibulum venenatis viverra proin at massa odio.Lorem ipsum dolor sit amet consectetur. Sed consequat ornare varius mauris facilisi aliquam a vel arcu. Rutrum vestibulum venenatis viverra proin at massa odio. Lorem ipsum dolor sit amet consectetur. Sed consequat ornare varius mauris facilisi aliquam a vel arcu. Rutrum vestibulum venenatis viverra proin at massa odio.Lorem ipsum dolor sit amet consectetur. Sed consequat ornare varius mauris facilisi aliquam a vel arcu. Rutrum vestibulum venenatis viverra proin at massa odio. Lorem ipsum dolor sit amet consectetur. Sed consequat ornare varius mauris facilisi aliquam a vel arcu. Rutrum vestibulum venenatis viverra proin at massa odio.Lorem ipsum dolor sit amet consectetur. Sed consequat ornare varius mauris facilisi aliquam a vel arcu. Rutrum vestibulum venenatis viverra proin at massa odio. Lorem ipsum dolor sit amet consectetur. Sed consequat ornare varius mauris facilisi aliquam a vel arcu. Rutrum vestibulum venenatis viverra proin at massa odio.';
   episodes: string [] = ['Episode One', '1 hour 30 minutes', '1']
+  actor: string [] = ["Director", "Nolan", "https://pics.filmaffinity.com/christopher_nolan-055100338198118-nm_large.jpg"]
+  dropdownCategories: string [] = [
+    "Season 1",
+    "Season 2",
+    "Season 3",
+  ]
 }
