f1627f8a8f488e3099bf5534e505707e44f88f6a
OmarJesusZuniga <omzuniga@estudiantec.cr>
2024-05-25 23:08:30 -0600

    Update Photos in DB

--- backend/modules/Photo.js
+++ backend/modules/Photo.js
@@ -1,12 +1,7 @@
 import supabase from '../config/supabaseClient.js';
+import { v4 as uuidv4 } from 'uuid';
 
-// Select all Photo entries
-export async function getAllPhotos() {
-    const { data, error } = await supabase
-        .from('Photo')
-        .select('id, name');
-    return { data, error };
-}
+const folder = 'imagesBD';
 
 // Select Photo by id
 export async function getPhotoById(id) {
@@ -14,31 +9,67 @@ export async function getPhotoById(id) {
         .from('Photo')
         .select('id, name')
         .eq('id', id);
-    return { data, error };
+
+    const path = data[0].name;
+
+    const { data: data1, error: error1 } = await supabase
+        .storage
+        .from(folder)
+        .getPublicUrl(path)
+
+    return { data, path : data1.publicUrl, error: error1 };
 }
 
 // Insert Photo
 export async function insertPhoto({name}) {
+    const path = await uploadImage(name);
+
+    if (path == null) {
+        return { data: null, error: "Error uploading image" };
+    }
+
     const { data, error } = await supabase
         .from('Photo')
-        .insert([{ name }]);
+        .insert([{ name: path }]);
+
     return { data, error };
 }
 
-// Update Photo by id
-export async function updatePhoto(id, {name}) {
-    const { data, error } = await supabase
-        .from('Photo')
-        .update({ name })
-        .eq('id', id);
-    return { data, error };
-}
-
-// Delete Photo by id
+// Delete Photo by id 
 export async function deletePhoto(id) {
     const { data, error } = await supabase
         .from('Photo')
         .delete()
-        .eq('id', id);
-    return { data, error };
+        .eq('id', id)
+        .select('name');
+
+    const path = data[0].name;
+
+    const { data: data1, error: error1 } = await supabase
+        .storage
+        .from(folder)
+        .remove([path])
+
+    return { data1, error1 };
+}
+
+async function uploadImage(file) {
+
+    if (file == null) {
+        return null;
+    }
+
+    const { data, error } = await supabase
+      .storage
+      .from(folder)
+      .upload('public/' + uuidv4(), file);
+
+    const path = data.path; 
+
+    const { data: ImportantData } = await supabase
+      .storage
+      .from(folder)
+      .getPublicUrl(path)
+  
+    return ImportantData.publicUrl;
 }
